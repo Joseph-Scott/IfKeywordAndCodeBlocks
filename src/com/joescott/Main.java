4f712f777219e5385f425e85a3db39c0d86d382b
Joseph Scott <jbs198585@gmail.com>
2019-03-03 15:17:10 -0500

    challenge question

---
+++ src/com/joescott/Main.java
@@ -0,0 +1,45 @@
+package com.joescott;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    boolean gameOver = true;
+	    int score = 800;
+	    int levelCompleted = 5;
+	    int bonus = 100;
+
+//	    if (score < 5000 && score > 1000) {
+//			System.out.println("Your score was less than 5000 but greater than 1000");
+//		} else if (score < 1000) {
+//			System.out.println("Your score was less than 1000");
+//	    } else {
+//			System.out.println("Got here");
+//
+//		}
+
+		if (gameOver) {
+			int finalScore = score + (levelCompleted * bonus);
+			System.out.println("Your final score was " + finalScore);
+		}
+
+        // Print out a second score on the screen with the following
+		// score set to 10000
+        // levelCompleted set to 8
+        // bonus set to 200
+        // But make sure the first printout above still displays as well
+
+		boolean nextGameOver = true;
+		int nextScore = 10000;
+		int nextLevelCompleted = 8;
+		int nextBonus = 200;
+
+		if (nextGameOver) {
+			int finalScore = nextScore + (nextLevelCompleted * nextBonus);
+			System.out.println("Your final score was " + finalScore);
+		}
+
+
+
+
+    }
+}
